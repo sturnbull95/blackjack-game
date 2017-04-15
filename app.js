2ea05be1f805a4bdd1c80ec414413429bb7c03c3
Steven Turnbull <sturnbull95@172-18-15-29.DYNAPOOL.NYU.EDU>
2017-04-15 14:29:04 -0400

    commiting with attempt on reset, not fully complete yet

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // app.js for homework #7 - blackjack
+var fs = require('fs');
+var express = require('express');
+var hbs = require('hbs');
+var expressSess = require("express-session");
+var mongoose = require('mongoose');
+const URLSlugs = require('mongoose-url-slugs');
+var bodyParser = require("body-parser");
+var app = express();
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+console.log(__dirname);
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/myapp/public/index.html');
+});
+app.get('/javascripts/main.js',function(req,res){
+  res.sendFile(__dirname + '/myapp/public/javascripts/main.js');
+});
+
+
+app.listen(3000);
---
+++ myapp/public/javascripts/main.js
@@ -0,0 +1,450 @@
+function main(){
+	//console.log(document.getElementById('startValues'));
+	var button = document.querySelector('.playBtn');
+	console.log(button);
+	button.addEventListener('click',function(e){
+		e.preventDefault();
+		document.querySelector('.start').style.display = 'none';
+		var x = document.getElementById("startValues").value;
+		initialVal = x.split(',');
+		player = [];
+		computer = [];
+		var winnerPrintOut = document.createElement('P');
+		winnerPrintOut.id = 'winnerPrint';
+		winnerPrintOut.style.display = 'none';
+		document.querySelector('.game').appendChild(winnerPrintOut);
+		var compImgClass = document.createElement('div');
+		compImgClass.className = 'computerIClass';
+		var computerImg = document.createElement('IMG');
+		computerImg.id = 'compImg';
+		var playerImg = document.createElement('IMG');
+		playerImg.id = 'playImg';
+		document.querySelector('.game').appendChild(computerImg);
+		document.querySelector('.game').appendChild(playerImg);
+		cards = generateCards();
+		playerTotal = calculateHand(player);
+		initialVal = [];
+		shuffledDeck = shuffle(cards);
+		var playerPrintOut = document.createElement('P');
+		playerPrintOut.id = 'playerPrint';
+		playerPrintOut.style.display = '';
+		var computerPrintOut = document.createElement('P');
+		computerPrintOut.id = 'computerPrint';
+		computerPrintOut.style.display = '';
+		document.querySelector('.game').appendChild(playerPrintOut);
+		document.querySelector('.game').appendChild(computerPrintOut);
+		var hitButton = document.createElement('input');
+		hitButton.type = 'button';
+		hitButton.id = 'btn_hit';
+		hitButton.value = 'Hit';
+		hitButton.style.display = '';
+		hitButton.addEventListener('click', function(e){
+			playerMove('h');});
+		document.querySelector('.game').appendChild(hitButton);
+		var standButton = document.createElement('input');
+		standButton.type = 'button';
+		standButton.id = 'btn_stand';
+		standButton.value = 'Stand';
+		standButton.style.display = '';
+		standButton.addEventListener('click', function(e){
+			setInterval(playerMove('s'),1000);});
+		document.querySelector('.game').appendChild(standButton);
+		//document.getElementById("winnerPrint").innerHTML = "The Winner is: " + determineWinner(playerTotal,computerTotal);
+		document.getElementById("playerPrint").innerHTML = "Your total is: " + playerTotal;
+		document.getElementById("computerPrint").innerHTML = "Computer total is: ?";
+		var resetButton = document.createElement('input');
+		resetButton.type = 'button';
+		resetButton.id = 'btn_reset';
+		resetButton.value = 'Reset';
+		resetButton.style.display = 'none';
+		resetButton.addEventListener('click', function(e){
+				resetGame();
+			});
+		document.querySelector('.game').appendChild(resetButton);
+		//document.querySelector('.game').appendChild(obj);
+	});
+};
+document.addEventListener('DOMContentLoaded', main);
+
+
+
+
+function findCard(card,deck){
+	for(var i = 0; i < deck.length; i++){
+		if(deck[i].face == card.face && deck[i].suit == card.suit){
+			return i;
+		}
+	}
+	return -1;
+}
+function resetGame(){
+	document.querySelector('.start').reset();
+	// document.querySelector('.start').style.display = '';
+	// document.getElementById('btn_reset').style.display = 'none';
+	// document.getElementById('computerPrint').style.display = 'none';
+	// document.getElementById('playerPrint').style.display = 'none';
+	// document.getElementById("startValues").value = '';
+}
+function generateCards() {
+	var suit = ['spades', 'hearts', 'diamonds', 'clubs'];
+	var face = ['2', '3', '4', '5', '6', '7',
+			'8', '9', '10', 'J', 'Q', 'K', 'A'];
+	for(var i = 0; i<initialVal.length; i++){
+		do{
+			var random = Math.floor((Math.random() * 4));
+			var thisSuit = suit[random];
+			var thisFace = initialVal[i];
+			var thisCard = {suit: thisSuit, face: thisFace};
+		}while(findCard(thisCard,computer) != -1 || findCard(thisCard,player) != -1);
+
+		if(i % 2){
+			player.push(thisCard);
+			document.getElementById('playImg').src = getImg(thisCard);
+		}
+		else{
+			computer.push(thisCard);
+		}
+	}
+	deck= [];
+	for (var i = 0; i < face.length; i++) {
+		for (var j = 0; j < suit.length; j++ ) {
+			var card = {
+				face: face[i],
+				suit: suit[j]
+			}
+			if(findCard(card,computer) != -1 || findCard(card,player) != -1){
+				continue;
+			}
+			deck.push(card);
+		}
+	}
+	return deck;
+}
+// FUNCTION TO GET IMG FOR CARD
+function getImg(card){
+	var img = '';
+	if(card.face == '2'){
+		if(card.suit == 'spades'){
+			img = "2_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "2_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "2_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "2_of_diamonds.svg";
+		}
+	}
+	if(card.face == '3'){
+		if(card.suit == 'spades'){
+			img = "3_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "3_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "3_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "3_of_diamonds.svg";
+		}
+	}
+	if(card.face == '4'){
+		if(card.suit == 'spades'){
+			img = "4_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "4_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "4_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "4_of_diamonds.svg";
+		}
+	}
+	if(card.face == '5'){
+		if(card.suit == 'spades'){
+			img = "5_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "5_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "5_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "5_of_diamonds.svg";
+		}
+	}
+	if(card.face == '6'){
+		if(card.suit == 'spades'){
+			img = "6_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "6_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "6_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "6_of_diamonds.svg";
+		}
+	}
+	if(card.face == '7'){
+		if(card.suit == 'spades'){
+			img = "7_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "7_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "7_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "7_of_diamonds.svg";
+		}
+	}
+	if(card.face == '8'){
+		if(card.suit == 'spades'){
+			img = "8_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "8_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "8_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "8_of_diamonds.svg";
+		}
+	}
+	if(card.face == '9'){
+		if(card.suit == 'spades'){
+			img = "9_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "9_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "9_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "9_of_diamonds.svg";
+		}
+	}
+	if(card.face == '10'){
+		if(card.suit == 'spades'){
+			img = "10_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "10_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "10_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "10_of_diamonds.svg";
+		}
+	}
+	if(card.face == 'J'){
+		if(card.suit == 'spades'){
+			img = "jack_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "jack_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "jack_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "jack_of_diamonds.svg";
+		}
+	}
+	if(card.face == 'Q'){
+		if(card.suit == 'spades'){
+			img = "queen_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "queen_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "queen_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "queen_of_diamonds.svg";
+		}
+	}
+	if(card.face == 'K'){
+		if(card.suit == 'spades'){
+			img = "king_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "king_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "king_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "king_of_diamonds.svg";
+		}
+	}
+	if(card.face == 'A'){
+		if(card.suit == 'spades'){
+			img = "ace_of_spades.svg";
+		}
+		if(card.suit == 'hearts'){
+			img = "ace_of_hearts.svg";
+		}
+		if(card.suit == 'clubs'){
+			img = "ace_of_clubs.svg";
+		}
+		if(card.suit == 'diamonds'){
+			img = "ace_of_diamonds.svg";
+		}
+	}
+	return img;
+}
+
+function shuffle(deck) {
+var shuffledDeck = new Array();
+var index;
+for (var i = 0; i < deck.length; i++) {
+	index = Math.floor(Math.random() * deck.length);
+	shuffledDeck.push(deck[index]);
+	deck.splice(index, 1);
+}
+return shuffledDeck;
+}
+
+function calculateHand(cards) {
+	var total = 0;
+	var length = cards.length;
+	var aceCount = 0;
+	for (var i = 0; i < length; i++){
+		if (cards[i].face == 'J' || cards[i].face == 'Q' ||
+			cards[i].face == 'K') {
+				total = total + 10;
+		}
+		else if (cards[i].face == 'A') {
+			total = total + 11;
+			aceCount++;
+		}
+		else {
+			total = total + parseInt(cards[i].face);
+		}
+	}
+	while (total > 21 && aceCount > 0) {
+		total = total - 10;
+		aceCount--;
+	}
+	return total;
+}
+function determineWinner(human, computer) {
+	var winner = '';
+	if (human > 21 || computer > 21) {
+		if (human > 21 && computer > 21) {
+			winner = "YOU BOTH BUST!";
+			document.getElementById('btn_stand').style.display = 'none';
+			document.getElementById('btn_hit').style.display = 'none';
+			return winner;
+		}
+		if (human <= 21) {
+			winner = "You win!";
+			document.getElementById('btn_stand').style.display = 'none';
+			document.getElementById('btn_hit').style.display = 'none';
+			return winner;
+		}
+		else {
+			winner = "Computer wins!";
+			document.getElementById('btn_stand').style.display = 'none';
+			document.getElementById('btn_hit').style.display = 'none';
+			return winner;
+		}
+	}
+	if (human==computer){
+		winner = "It's a tie!!!";
+		document.getElementById('btn_stand').style.display = 'none';
+		document.getElementById('btn_hit').style.display = 'none';
+		return winner;
+	}
+	else {
+		var humanDif = 21-human;
+		var compDif = 21-computer;
+		if (humanDif>compDif) {
+			winner = 'Computer wins!';
+			document.getElementById('btn_stand').style.display = 'none';
+			document.getElementById('btn_hit').style.display = 'none';
+		}
+		else {
+			winner = "You win!";
+			document.getElementById('btn_stand').style.display = 'none';
+			document.getElementById('btn_hit').style.display = 'none';
+		}
+	}
+	return winner;
+}
+
+function playerMove(move){
+	switch (move) {
+		case 'h':
+			// Deal one card, calculate value, remove from deck
+				player.push(shuffledDeck[0]);
+				playerTotal = calculateHand(player);
+				shuffledDeck.splice(0, 1);
+				console.log("playerTotal: " + playerTotal);
+				document.getElementById('playerPrint').innerHTML = "Your total is: " + playerTotal;
+				// select DOM object responsible for player score (div/span with value inside)
+				if(playerTotal > 21){
+					document.getElementById('playerPrint').innerHTML = 'You Busted';
+					document.getElementById('btn_hit').style.display = 'none';
+					document.getElementById('btn_stand').style.display = 'none';
+					document.getElementById('btn_reset').style.display = '';
+					return;
+				}
+			break;
+		// If "stay", break from loop
+		case 's':
+			stay = true;
+			// begin computer turn
+			setTimeout(startAI,1000);
+			document.getElementById('btn_hit').style.display = 'none';
+			document.getElementById('btn_stand').style.display = 'none';
+			document.getElementById('btn_reset').style.display = '';
+			document.getElementById(determineWinner(playerTotal,computerTotal));
+			break;
+		default:
+			alert('Unknown Value ' + move);
+			break;
+
+	}
+}
+
+//GAME STARTS
+var initialVal = false;
+var player = false;
+var computer = false;
+var cards = false;
+var shuffledDeck = false;
+var playerTotal = false;
+var computerTotal = 0;
+function startAI(){
+	stay = false;
+	while (!stay) {
+		computerTotal = calculateHand(computer);
+		document.getElementById("computerPrint").innerHTML = "Computer total is: " + computerTotal;
+		console.log("computerTotal: " + computerTotal);
+		if (computerTotal >= 17 && computerTotal <= 21) {
+			stay = true;
+			break;
+		}
+		if(computerTotal > 21){
+			document.getElementById("computerPrint").innerHTML = "Computer Busted";
+			break;
+		}
+		computer.push(shuffledDeck[0]);
+		shuffledDeck.splice(0, 1);
+	}
+}
